7ab98881bd5acf31eb08437a7bed3b8768e18e81
Mohammed Misri <mdmisri586@gmail.com>
2023-06-30 17:23:15 +0530

    update

--- src/components/GymGallery.js
+++ src/components/GymGallery.js
@@ -1,30 +1,33 @@
 import React from 'react';
-
-//import data
 import { workouts } from '../data';
-
-// import components
 import WorkoutSlider from './GymGallerySlider';
 
 const Workouts = () => {
-  // destructure workouts data
   const { title, icon } = workouts;
-  return(
-     <section id='gymgallery' className='section'>
-    {/* section title */ }
-    <div className= 'section-title-group max-w[540px]
-    mx-auto px-4 lg:px-0' 
-    data-aos='fade-up'
-    data-aos-delay='200'>
-    <img src={icon} alt='' />
-    <h2 className='h2 section-title'>{title}<span className='text-primary-200'>.</span>
-    </h2>
-    </div>
-    {/* slider  */}
-    <div data-aos='fade-up'
-    data-aos-delay='300'>
-      <WorkoutSlider/>
-    </div>
+
+  return (
+    <section id="gymgallery" className="section">
+      <div className="flex flex-wrap">
+        <div className="w-full h-auto">
+          {/* section title */}
+          <div
+            className="section-title-group max-w-[540px] mx-auto px-4 lg:px-0"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <img src={icon} alt="" />
+            <h2 className="h2 section-title">
+              {title}
+              <span className="text-primary-200">.</span>
+            </h2>
+          </div>
+        </div>
+      </div>
+
+      {/* slider */}
+      <div data-aos="fade-up" data-aos-delay="300">
+        <WorkoutSlider />
+      </div>
     </section>
   );
 };
