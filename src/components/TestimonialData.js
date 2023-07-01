f0efb220adf6263a0cf468c52edaed52c753c47c
Mohammed Misri <mdmisri586@gmail.com>
2023-07-01 23:30:19 +0530

    updateyarn add gh-pages

--- src/components/TestimonialData.js
+++ src/components/TestimonialData.js
@@ -1,52 +1,36 @@
 import React from 'react';
 
-// import CommunityIcn from '../src/assets/img/community/icons';
-// import I1 from "./photos/img1.png"
 export const testimonialData = [
-  // icon: CommunityIcn,
-
-
   {
     id: 1,
     name: 'Mohammed K.',
-    // image: {I1},
-    quote: '“Gym is Well maintained. Coach is very kind, a professional body builder and ofcourse a dietitian too. He makes everyone to focus on each muscle of their body throughout a week. A Good inspiration for our younger generation.”',
+    quote: '“Gym is Well maintained. Coach is very kind, a professional body builder and of course a dietitian too. He makes everyone focus on each muscle of their body throughout a week. A Good inspiration for our younger generation.”',
   },
   {
     id: 2,
-    name: 'hameed M',
-    // image: {I1},
-    quote: '“Awesome gym by Esa Misri sahab equipment is also very good.”',
+    name: 'Hameed M',
+    quote: '“Awesome gym by Esa Misri Sahab. Equipment is also very good.”',
   },
   {
     id: 3,
     name: 'Salman A.',
-    // image: {I1},
-    quote: '“Have been a member since a long time , one of the best gym in old city and won’t Burn a hole in your pocket very affordable . Awesome equipment can go for it undoubtedly, strongly recommend.”',
+    quote: '“Have been a member since a long time, one of the best gyms in the old city and won’t burn a hole in your pocket. Very affordable. Awesome equipment, can go for it undoubtedly. Strongly recommend.”',
   },
-  
-];// i am writing some points as comments for claiity ok
+];
 
-export const Testimonial = () => {    //function which maps the images from the data--
-  //func to be exported
-  //chalo ye file all clear hai
+export const Testimonial = () => {
   return (
     <div className="flex items-center justify-center py-8 bg-gray-100">
       {testimonialData.map((testimonial) => (
         <div
           key={testimonial.id}
-          className="w-1/3 p-6 mx-4 bg-white rounded-lg shadow-lg"  // id
+          className="w-1/3 p-6 mx-4 bg-white rounded-lg shadow-lg"
         >
-          <img
-            src={testimonial.image}                                 //image
-            alt={testimonial.name}
-            className="w-20 h-20 rounded-full mx-auto mb-4"
-          />    
-          <p className="italic text-gray-700">{testimonial.quote}</p>   
-        </div>                                                //info
+          <p className="italic text-gray-700">{testimonial.quote}</p>
+        </div>
       ))}
     </div>
   );
 };
 
-export default testimonialData;  //data to be exported in Testimonial.js
+export default testimonialData;
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -9,13 +9,13 @@ const Testimonials = () => {
   return (
     <section id='testimonials' className='bg-pink-100 section'>
       {/* section title */}
-      <div className='section-title-group justify-start' 
+      <div className='section-title-group  max-w-[540px] mx-auto px-4 lg:px-0'
       data-aos='fade-up' 
-      data-aos-delay='100' //
+      data-aos-delay='200' //
       >
         {/* <img src={icon} alt=''/> */}
-        <h2 className='h2 section-title' >Testinomials
-          
+        <h2 className='h2 section-title' >Testimonials
+        <span className="text-primary-200">.</span>
           </h2>
       </div>
       <div className="flex flex-wrap justify-center py-8">
@@ -24,12 +24,11 @@ const Testimonials = () => {
             key={testimonial.id}
             className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-6 mx-4 bg-white rounded-lg shadow-lg"
           >
-            <img
-              src={testimonial.image}
+            {/* <h3
               alt={testimonial.name}
               className="w-20 h-20 rounded-full mx-auto mb-4"
-            />
-            {/* <h3 className="text-lg font-semibold mb-2">{testimonial.name}</h3> */}
+            /> */}
+            <h2 className="text-lg font-semibold mb-2">-{testimonial.name}</h2>
             <p className="italic text-gray-700">{testimonial.quote}</p>
           </div>
         ))}
