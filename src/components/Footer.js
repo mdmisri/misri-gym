69b8cf8ad3c7869bdb135c4a09f0b273df72db22
Mohammed Misri <93259254+mdmisri@users.noreply.github.com>
2023-07-01 23:58:46 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
   return ( 
   <footer className='bg-neutral-500 h-[100px] md:h-[120px]px-[10px]'>
     <div
-     className='container mx-auto h-full flex justify-center items-center md:items-end md:pb-[50px]
+     className='container mx-auto h-full flex justify-center items-center md:items-end md:pb-[30px]
     ' 
-    data-aos='fade-up' 
-    data-aos-delay='200'
+    // data-aos='fade-up' 
+    // data-aos-delay='200'
     >
       {/*contact */}
      <p className='text-neutral-300  text-xl'>{copyrightText}</p>
