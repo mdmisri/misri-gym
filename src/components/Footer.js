4c6339963e682b540cafbbc56a7d27ee6df93606
Mohammed Misri <mdmisri586@gmail.com>
2023-07-02 02:10:33 +0530

    ff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,13 +8,13 @@ const Footer = () => {
   return ( 
   <footer className='bg-neutral-500 h-[100px] md:h-[120px]px-[10px]'>
     <div
-     className='container mx-auto h-full flex justify-center items-center md:items-end md:pb-[35px]
+     className='container mx-auto h-full flex justify-center items-center md:items-end md:pb-[50px]
     ' 
     // data-aos='fade-up' 
     // data-aos-delay='200'
     >
       {/*contact */}
-     <p className='text-neutral-300  text-xl'>{copyrightText}</p>
+     <p className='text-neutral-300  text-lg'>{copyrightText}</p>
     </div>
   </footer>
   );
